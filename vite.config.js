2dd793a6c475149b9c160d7c0525bd232db53c12
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2023-08-07 17:28:15 +0700

    update vite and image

--- vite.config.js
+++ vite.config.js
@@ -17,37 +17,37 @@ export default defineConfig({
           "D'Catet: Lupakan Kebiasan Sering Kelupaan dengan mencatat hal hal penting",
         screenshots: [
           {
-            src: "/assets/Screenshoot-1.png",
+            src: "/img/Screenshoot-1.png",
             sizes: "1280x720",
             type: "image/png",
           },
           {
-            src: "/assets/Screenshoot-2.png",
+            src: "/img/Screenshoot-2.png",
             sizes: "1280x720",
             type: "image/png",
           }
         ],
         icons: [
           {
-            src: "/assets/icon-192x192.png",
+            src: "/img/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "/assets/icon-256x256.png",
+            src: "/img/icon-256x256.png",
             sizes: "256x256",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "/assets/icon-384x384.png",
+            src: "/img/icon-384x384.png",
             sizes: "384x384",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "/assets/icon-512x512.png",
+            src: "/img/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
